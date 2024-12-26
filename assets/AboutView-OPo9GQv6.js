18e261dd242f14184c0ce839f2ebe32e7353188d
Shykan <shykadev@gmail.com>
2024-12-26 16:56:38 -0600

    Updates

---
+++ assets/AboutView-OPo9GQv6.js
@@ -0,0 +1,1 @@
+import{J as o,K as s,a as t,o as a}from"./index-BB3XffTC.js";const n={},r={class:"about"};function c(i,e){return a(),s("div",r,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",c]]);export{l as default};
